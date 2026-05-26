a3974d08f1ac8ab918b83ec5a61d5e1321433f5a
sanvibaasri <sanvibaasri@gmail.com>
2026-05-26 13:04:31 +0530

    variables

--- tax_calculate.js
+++ tax_calculate.js
@@ -1,5 +1,20 @@
 document.getElementById('calc-btn').addEventListener('submit', (e) =>{
     const salary = document.getElementById('grossSalary');
+    const interestInc = document.getElementById('IncFromInterest');
+    const rentInc = document.getElementById('RentalInc');
+    const digitalInc = document.getElementById('DigitalInc');
+    const homeLoan = document.getElementById('HomeLoanInc');
+    const profTax = document.getElementById('ProfTax');
+    const otherInc = document.getElementById('OtherIncome');
+    const basicDed = document.getElementById('BasicDed');
+    const medInsurace = document.getElementById('MedInsurance');
+    const houseLoan = document.getElementById('HousingLoan');
+    const Nps = document.getElementById('NPS');
+    const charity = document.getElementById('Charity');
+    const otherDed = document.getElementById('OtherDed');
+    
+
+
     
 
 })
