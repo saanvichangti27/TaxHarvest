512e8500e6d8778e9a10cf507612dda1bd4650f9
saanvi <saanvichangti27@gmail.com>
2026-05-29 15:48:08 +0530

    added commas

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ import * as NewReg from "./new_regime.js";
 import { getSavingsTips } from "./savings.js";
 
 document.getElementById("calc-btn").addEventListener("click", (e) => {
-    const salary = parseInt(document.getElementById("grossSalary").value) || 0;
+    const salary = parseInt(document.getElementById("grossSalary").value.replace(/,/g,'')) || 0;
     let interestInc =
-        parseInt(document.getElementById("IncFromInterest").value) || 0;
-    const rentInc = parseInt(document.getElementById("RentalInc").value) || 0;
-    const profTax = parseInt(document.getElementById("ProfTax").value) || 0;
-    const otherInc = parseInt(document.getElementById("OtherIncome").value) || 0;
-    const basicDed = parseInt(document.getElementById("BasicDed").value) || 0;
+        parseInt(document.getElementById("IncFromInterest").value.replace(/,/g,'')) || 0;
+    const rentInc = parseInt(document.getElementById("RentalInc").value.replace(/,/g,'')) || 0;
+    const profTax = parseInt(document.getElementById("ProfTax").value.replace(/,/g,'')) || 0;
+    const otherInc = parseInt(document.getElementById("OtherIncome").value.replace(/,/g,'')) || 0;
+    const basicDed = parseInt(document.getElementById("BasicDed").value.replace(/,/g,'')) || 0;
     const medInsurance =
-        parseInt(document.getElementById("MedInsurance").value) || 0;
-    const houseLoan = parseInt(document.getElementById("HousingLoan").value) || 0;
-    const HRA = parseInt(document.getElementById("NPS").value) || 0;
-    const charity = parseInt(document.getElementById("Charity").value) || 0;
-    const otherDed = parseInt(document.getElementById("OtherDed").value) || 0;
+        parseInt(document.getElementById("MedInsurance").value.replace(/,/g,'')) || 0;
+    const houseLoan = parseInt(document.getElementById("HousingLoan").value.replace(/,/g,'')) || 0;
+    const HRA = parseInt(document.getElementById("NPS").value.replace(/,/g,'')) || 0;
+    const charity = parseInt(document.getElementById("Charity").value.replace(/,/g,'')) || 0;
+    const otherDed = parseInt(document.getElementById("OtherDed").value.replace(/,/g,'')) || 0;
     const city = document.getElementById("citySelect").value;
 
     if (interestInc > 10000) interestInc = interestInc - 10000;
@@ -72,36 +72,36 @@ document.getElementById("calc-btn").addEventListener("click", (e) => {
     let cessOld = Math.max(0, Math.round(0.04 * payableTaxOld));
     let cessNew = Math.max(0, Math.round(0.04 * payableTaxNew));
 
-    document.getElementById("grossIncNew").textContent = Math.round(totalInc);
-    document.getElementById("res-gross-old").textContent = Math.round(totalInc);
+    document.getElementById("grossIncNew").textContent = Math.round(totalInc).toLocaleString('en-IN');
+    document.getElementById("res-gross-old").textContent = Math.round(totalInc).toLocaleString('en-IN');
 
     // Wrap these in a condition
-    document.getElementById("res-std-new").textContent = totalInc > 0 ? 75000 : 0;
-    document.getElementById("res-std-old").textContent = totalInc > 0 ? 50000 : 0;
+    document.getElementById("res-std-new").textContent = totalInc > 0 ? (NewReg.stdDed).toLocaleString('en-IN') : 0;
+    document.getElementById("res-std-old").textContent = totalInc > 0 ? (OldReg.stdDed).toLocaleString('en-IN') : 0;
 
     document.getElementById("res-ded-new").textContent = 0;
     document.getElementById("res-ded-old").textContent =
-        totalInc > 0 ? Math.round(totalDedOld) : 0;
+        totalInc > 0 ? Math.round(totalDedOld).toLocaleString('en-IN') : 0;
     document.getElementById("res-taxable-new").textContent =
-        Math.round(taxableIncNew);
+        Math.round(taxableIncNew).toLocaleString('en-IN');
     document.getElementById("res-taxable-old").textContent =
-        Math.round(taxableIncOld);
+        Math.round(taxableIncOld).toLocaleString('en-IN');
 
-    document.getElementById("res-tax-new").textContent = Math.round(incomeTaxNew);
-    document.getElementById("res-tax-old").textContent = Math.round(incomeTaxOld);
+    document.getElementById("res-tax-new").textContent = Math.round(incomeTaxNew).toLocaleString('en-IN');
+    document.getElementById("res-tax-old").textContent = Math.round(incomeTaxOld).toLocaleString('en-IN');
 
-    document.getElementById("res-rebate-new").textContent = Math.round(rebateNew);
-    document.getElementById("res-rebate-old").textContent = Math.round(rebateOld);
+    document.getElementById("res-rebate-new").textContent = Math.round(rebateNew).toLocaleString('en-IN');
+    document.getElementById("res-rebate-old").textContent = Math.round(rebateOld).toLocaleString('en-IN');
 
-    document.getElementById("res-cess-new").textContent = Math.round(cessNew);
-    document.getElementById("res-cess-old").textContent = Math.round(cessOld);
+    document.getElementById("res-cess-new").textContent = Math.round(cessNew).toLocaleString('en-IN');
+    document.getElementById("res-cess-old").textContent = Math.round(cessOld).toLocaleString('en-IN');
 
     document.getElementById("res-payable-new").textContent = Math.round(
         payableTaxNew + cessNew,
-    );
+    ).toLocaleString('en-IN');
     document.getElementById("res-payable-old").textContent = Math.round(
         payableTaxOld + cessOld,
-    );
+    ).toLocaleString('en-IN');
 
 
      const tips = getSavingsTips(basicDed, medInsurance, HRA, charity, payableTaxOld);
